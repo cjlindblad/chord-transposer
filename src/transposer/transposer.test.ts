de09550419d4c7287c8dd79ee342afb51c5d9857
cj lindblad <carljohan.lindblad@gmail.com>
2019-11-14 20:42:19 +0100

    Transposes some real chord progressions

--- src/transposer/transposer.test.ts
+++ src/transposer/transposer.test.ts
@@ -4,6 +4,8 @@ import { Note } from '../note/note';
 import { NoteName } from '../note/noteName';
 import { Interval } from '../interval/interval';
 import { Scale } from '../scale/scale';
+import { Mode } from '../scale/mode';
+import { NoteAlteration } from '../note/noteAlteration';
 
 describe('chord progression transposer', () => {
   it('transposes C G Am F in C major to G major', () => {
@@ -26,5 +28,95 @@ describe('chord progression transposer', () => {
       .join('');
 
     expect(transposedProgressionString).toEqual('GDEmC');
+  });
+
+  it('transposes Imagine bridge from C major to B major', () => {
+    const progression = [
+      Chord.from(Note.from(NoteName.F)),
+      Chord.from(Note.from(NoteName.G)),
+      Chord.from(Note.from(NoteName.C)),
+      Chord.from(Note.from(NoteName.E))
+    ];
+
+    const fromScale = Scale.from(Note.from(NoteName.C));
+    const toScale = Scale.from(Note.from(NoteName.B));
+
+    const transposer = new Transposer(progression, fromScale);
+
+    const transposedProgression = transposer.transpose(toScale);
+
+    const transposedProgressionString = transposedProgression
+      .map(chord => chord.toString())
+      .join('');
+
+    expect(transposedProgressionString).toEqual('EF#BD#');
+  });
+
+  it('transposes "Värmlandsvisan" from Dm to Am', () => {
+    const Dm = Chord.from(Note.from(NoteName.D), {
+      third: Interval.MinorThird
+    });
+    const A7 = Chord.from(Note.from(NoteName.A), {
+      third: Interval.MajorThird,
+      seventh: Interval.MinorSeventh
+    });
+    const Bb = Chord.from(Note.from(NoteName.B, NoteAlteration.Flat));
+    const C7 = Chord.from(Note.from(NoteName.C), {
+      seventh: Interval.MinorSeventh
+    });
+    const F = Chord.from(Note.from(NoteName.F));
+    const Gm = Chord.from(Note.from(NoteName.G), {
+      third: Interval.MinorThird
+    });
+    const G7 = Chord.from(Note.from(NoteName.G), {
+      seventh: Interval.MinorSeventh
+    });
+    const A = Chord.from(Note.from(NoteName.A));
+
+    const progression = [
+      Dm,
+      A7,
+      Bb,
+      C7,
+      F,
+      Gm,
+      A7,
+      Dm,
+      G7,
+      A7,
+      Dm,
+      C7,
+      F,
+      Gm,
+      A7,
+      Dm,
+      A,
+      Dm,
+      A7,
+      Bb,
+      C7,
+      F,
+      F,
+      A7,
+      Dm,
+      G7,
+      A7,
+      Dm
+    ];
+
+    const fromScale = Scale.from(Note.from(NoteName.D), Mode.Aeolian);
+    const toScale = Scale.from(Note.from(NoteName.A), Mode.Aeolian);
+
+    const transposer = new Transposer(progression, fromScale);
+
+    const transposedProgression = transposer.transpose(toScale);
+
+    const transposedProgressionString = transposedProgression
+      .map(chord => chord.toString())
+      .join(' ');
+
+    expect(transposedProgressionString).toEqual(
+      'Am E7 F G7 C Dm E7 Am D7 E7 Am G7 C Dm E7 Am E Am E7 F G7 C C E7 Am D7 E7 Am'
+    );
   });
 });
